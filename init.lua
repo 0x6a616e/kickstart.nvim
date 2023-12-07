7088f8420c8d6ea2ca3c06b6b2dafa61ee41ee45
0x6a616e <janel.fantasma2002@gmail.com>
2023-12-06 23:30:38 -0600

    feat: imported my custom settings

--- init.lua
+++ init.lua
@@ -72,5 +72,9 @@ require 'lsp-setup'
 -- (completion)
 require 'cmp-setup'
 
+-- [[ Configure vim-fugitive ]]
+-- (git integration)
+require 'fugitive-setup'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,17 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Custom keymaps
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex, { desc = "See current directory" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to clipboard" })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
