5649e2f7342d1bb1e490b3a76982c3a5fe3e2c3c
Jan <janel.fantasma2002@gmail.com>
2024-07-16 02:34:54 -0600

    feat: added oil.nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,7 +5,6 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Custom keymaps
-vim.keymap.set("n", "<leader>p", vim.cmd.Ex, { desc = "See current directory" })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
